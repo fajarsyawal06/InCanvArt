28d1f1a76309901f8e4ef728dc2a282b1e5802ac
fajarsyawal06 <ahmadfajarsyawal06@gmail.com>
2025-12-18 00:24:59 +0800

    Mengubah logo tab

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>InCanvArt</title>
 <link rel="stylesheet" href="/resources/css/input.css">
-<link rel="icon" type="image/png" href="{{ asset('images/LogoInCanvArt.png') }}">
+<link rel="icon" type="image/png" href="{{ asset('images/logoincanvart.png') }}">
 <script src="https://cdn.tailwindcss.com"></script>
 <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
 <link rel="stylesheet" href="{{ asset('css/flash.css') }}">
