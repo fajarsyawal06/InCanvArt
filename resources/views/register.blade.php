e3e2f1bf995ff718bd691748533b93dc02761c70
fajarsyawal06 <ahmadfajarsyawal06@gmail.com>
2025-12-18 00:27:59 +0800

    Mengubah logo tab login dan register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,9 +2,7 @@
 <html lang="id">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Register | InCanvArt</title>
+    <x-header></x-header>
 
     <!-- PAKAI CSS LOGIN YANG SAMA -->
     <link rel="stylesheet" href="{{ asset('css/loginregister.css') }}" />
