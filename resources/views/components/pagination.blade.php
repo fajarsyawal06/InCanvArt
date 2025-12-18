89f7e8e38030cbeb37ea3480aa555e51c7f5dd47
fajarsyawal06 <ahmadfajarsyawal06@gmail.com>
2025-12-19 04:26:38 +0800

    Perubahan Pada Statistik 2

--- resources/views/components/pagination.blade.php
+++ resources/views/components/pagination.blade.php
@@ -1,70 +1,56 @@
 @if ($paginator->hasPages())
-    <nav class="stats-pagination" aria-label="Page navigation">
+    <nav class="stats-pagination" role="navigation" aria-label="Pagination Navigation">
         <ul class="stats-pagination-list">
-            {{-- Previous --}}
-            <li>
-                @if ($paginator->onFirstPage())
-                    <span class="stats-page-link stats-page-link-disabled">
-                        Previous
-                    </span>
-                @else
-                    <a
-                        href="{{ $paginator->previousPageUrl() }}"
-                        rel="prev"
-                        class="stats-page-link">
-                        Previous
-                    </a>
-                @endif
-            </li>
 
-            {{-- Numbered pages --}}
+            {{-- Previous Page Link --}}
+            @if ($paginator->onFirstPage())
+                <li>
+                    <span class="stats-page-link stats-page-link-disabled">Previous</span>
+                </li>
+            @else
+                <li>
+                    <a class="stats-page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">Previous</a>
+                </li>
+            @endif
+
+            {{-- Pagination Elements --}}
             @foreach ($elements as $element)
-                {{-- "..." --}}
+
+                {{-- "Three Dots" Separator --}}
                 @if (is_string($element))
                     <li>
-                        <span class="stats-page-link stats-page-link-dots">
-                            {{ $element }}
-                        </span>
+                        <span class="stats-page-link stats-page-link-dots">{{ $element }}</span>
                     </li>
                 @endif
 
-                {{-- Links --}}
+                {{-- Array Of Links --}}
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
-                        <li>
-                            @if ($page == $paginator->currentPage())
-                                <span
-                                    class="stats-page-link stats-page-link-active"
-                                    aria-current="page">
-                                    {{ $page }}
-                                </span>
-                            @else
-                                <a
-                                    href="{{ $url }}"
-                                    class="stats-page-link">
-                                    {{ $page }}
-                                </a>
-                            @endif
-                        </li>
+                        @if ($page == $paginator->currentPage())
+                            <li>
+                                <span class="stats-page-link stats-page-link-active">{{ $page }}</span>
+                            </li>
+                        @else
+                            <li>
+                                <a class="stats-page-link" href="{{ $url }}">{{ $page }}</a>
+                            </li>
+                        @endif
                     @endforeach
                 @endif
+
             @endforeach
 
-            {{-- Next --}}
-            <li>
-                @if ($paginator->hasMorePages())
-                    <a
-                        href="{{ $paginator->nextPageUrl() }}"
-                        rel="next"
-                        class="stats-page-link">
-                        Next
-                    </a>
-                @else
-                    <span class="stats-page-link stats-page-link-disabled">
-                        Next
-                    </span>
-                @endif
-            </li>
+            {{-- Next Page Link --}}
+            @if ($paginator->hasMorePages())
+                <li>
+                    <a class="stats-page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">Next</a>
+                </li>
+            @else
+                <li>
+                    <span class="stats-page-link stats-page-link-disabled">Next</span>
+                </li>
+            @endif
+
         </ul>
     </nav>
 @endif
