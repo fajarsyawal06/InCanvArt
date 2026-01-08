1c3b80c8ab7b2293e5709b5135a0b12ed98a25cf
fajarsyawal06 <ahmadfajarsyawal06@gmail.com>
2026-01-08 09:57:16 +0800

    Admin Index

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -21,11 +21,6 @@
                         <div style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" class="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"></div>
                     </div>
                     <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-                        <div class="hidden sm:mb-8 sm:flex sm:justify-center">
-                            <div class="relative rounded-full px-3 py-1 text-sm/6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
-                                Announcing our next round of funding. <a href="#" class="font-semibold text-indigo-400"><span aria-hidden="true" class="absolute inset-0"></span>Read more <span aria-hidden="true">&rarr;</span></a>
-                            </div>
-                        </div>
                         <div class="text-center">
                             <h1 class="text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">Selamat Datang {{ auth()->user()->username }}</h1>
                             <p class="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">Bebaskan imajinasimu di kanvas digital tanpa batas. InCanvArt adalah aplikasi untuk seniman digital yang menghadirkan ruang kerja luas, alat kreatif intuitif, dan komunitas inspiratif. Buat, bagikan, dan jelajahi karya seni Anda — semua dalam satu platform.</p>
