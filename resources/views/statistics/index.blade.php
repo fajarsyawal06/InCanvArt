e32c35c62ed6b2e615946316a18bbe9d57ac5772
fajarsyawal06 <ahmadfajarsyawal06@gmail.com>
2026-01-13 11:08:34 +0800

    Mengganti file statistic

--- resources/views/statistics/index.blade.php
+++ resources/views/statistics/index.blade.php
@@ -261,7 +261,10 @@
 
         </div>
     </div>
-
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <x-flash></x-flash>
 </body>
 
 </html>
